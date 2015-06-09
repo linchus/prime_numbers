adea784b2007578c192441b9a1d69f57def48211
Sergey Sein <linchus@gmail.com>
2015-06-09 20:12:55 +0300

    Improve output perfomance

--- primary_matrix.rb
+++ primary_matrix.rb
@@ -2,14 +2,13 @@ matrix_size = ARGV.first.to_i
 abort('Matrix size should be greater then 0') unless matrix_size > 0
 
 require_relative 'lib/my_prime'
-require 'terminal-table'
 
 numbers = MyPrime.new.take(matrix_size)
-data = [[''] + numbers]
+cell_size = (numbers.last ** 2).to_s.length
+row_mask = " %#{cell_size}s |" * (matrix_size + 1)
+puts format(row_mask, '', *numbers)
 numbers.each do |prime|
-  data << numbers.map { |element| element * prime }.unshift(prime)
+  puts format(row_mask, prime, *numbers.map { |element| element * prime })
 end
-
-puts Terminal::Table.new rows: data
 
 
